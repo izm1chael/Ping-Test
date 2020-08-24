21a7188de454e790889db7f401b0de2946426808
Mike Johnson <m1chael.johnson1804@gmail.com>
2020-08-24 17:41:06 +0100

    Update pingtest.sh

--- pingtest.sh
+++ pingtest.sh
@@ -1,18 +1,26 @@
-#!/usr/local/bin/bash
 
+#!/usr/local/bin/bash
+ 
 echo -n "Eneter the name of your output file [ENTER]: "
         read LOG_OUT
 echo -n "Enter the file name with your domains in [ENTER]: "
          read SOURCE_FILE
 
-echo "Connectivity Test Results" > $LOG_OUT
-date > $LOG_OUT
+echo "Connectivity Test Results" >> $LOG_OUT
+date >> $LOG_OUT
+
+
+
 for host in $(cat $SOURCE_FILE ); do 
-	fping -u $host >& /dev/null
-    if [ $? -eq 0 ]; then
-        echo "$host is Alive"  > $LOG_OUT
-    else 
-    	echo "$host is DOWN" > $LOG_OUT
+    status_code=$(curl -m 2.5 -s --write-out '%{http_code}\n' "$host" -o /dev/null ) > /dev/null
+    if  [[ $status_code == *"200"* ]]; then
+        echo -e  "$host is Alive"  >> $LOG_OUT
+    elif [[ $status_code == *"301"* ]]; then
+    	 echo -e "$host is Permanently Redirected" >> $LOG_OUT
+   	elif [[ $status_code == *"302"* ]]; then
+    	 echo -e "$host is Temporarily Redirected" >> $LOG_OUT
+    else  
+        echo -e "$host is Down" >> $LOG_OUT
 
     fi
 done
